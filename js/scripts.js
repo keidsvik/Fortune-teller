bcbb94a47579070c945c29d9012934674ede83e2
William Pratt <willpratt78@gmail.com>
2020-03-31 14:36:53 -0700

    create if statement and finish writing out checkb git add .

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,36 @@ $(document).ready(function() {
   $('form#fortune-quiz').submit(function(event) {
     event.preventDefault();
     $('#UI').show();
-    $('input:checkbox[name=zodiac]:checked').each(function() {
-      var zodiacSign = $(this).val();
-      $('#UI').append(zodiacSign + '<br>');
-    
-    var result;
 
-    if (zodiacSign === 'fire') {
-      result = $('#lucky').show();
-    }
+    var zodiacSign;
+    var goodLuck;
+    var badLuck;
+    var zodiacCounter;
+    var luckCounter;
+    var badLuckCounter;
+
+    $('input:checkbox[name=zodiac]:checked').each(function() {
+      zodiacSign = $(this).val();
+      $('#UI').append(zodiacSign + '<br>');
+      parseInt(zodiacCounter ++);
+      console.log(zodiacCounter);
     });
-    $('#fortune-quiz').hide();
+    $('input:checkbox[name=good]:checked').each(function() {
+      goodLuck =$(this).val();
+      $('#UI').append(goodLuck + '<br>');
+      parseInt(luckCounter ++);
+    });
+    $('input:checkbox[name=bad]:checked').each(function() {
+      badLuck=$(this).val();
+      $('#UI').append(badLuck + "<br>");
+      parseInt(badLuckCounter ++)
+    });
+
+    var result;
+    
+    if (zodiacSign) {
+      result = $('#lucky').show();
+      $('#fortune-quiz').hide();
+    };
   });
 });
